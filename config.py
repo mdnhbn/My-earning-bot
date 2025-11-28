928baf4cd912c0217ce4b2c34469bff6ca6ba246
Md Nazmul Hassan Bhuiyan <38237787+mdnhbn@users.noreply.github.com>
2025-11-28 14:38:58 +0300

    Add configuration settings for bot and channels

---
+++ config.py
@@ -0,0 +1,14 @@
+# config.py
+
+TOKEN = "YOUR_BOTFATHER_TOKEN"   # BotFather থেকে পাওয়া টোকেন এখানে বসাও
+ADMIN_IDS = [123456789]          # এডমিন টেলিগ্রাম ইউজার আইডি বসাও
+REQUIRED_CHANNELS = [
+    "@channel1",
+    "@channel2",
+    "@channel3",
+    "@channel4",
+    "@channel5"
+]
+
+VACATION_MODE = False
+MAINTENANCE_MODE = False
